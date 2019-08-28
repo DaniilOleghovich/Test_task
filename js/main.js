3a9fba4dbf30c9195e5176ff428409fc1dd09ce5
DaniilOleghovich <danya.pavlichenko.1997@mail.ru>
2019-08-28 13:52:01 +0300

    .

--- js/main.js
+++ js/main.js
@@ -1,51 +1,3 @@
-// let moment = require('moment');
-
-// function tplawesome(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){return t[n][r]})}return res}
-//
-//
-// $(function() {
-//     $("#search2").on("click", function(e) {
-//         e.preventDefault();
-//         // prepare the request
-//         var request = gapi.client.youtube.search.list({
-//             part: "snippet",
-//             type: "video",
-//             q: "Javascript|Python -basics",
-//             // videoCategoryId: 28,            //27- Образование, 28- Наука и техника
-//             maxResults: 15,
-//             order: "date",
-//             publishedAfter: "2019-04-08T00:00:00Z"
-//         });
-//         request.execute(function(response) {
-//             var results = response.result;
-//             $("#results").html("");
-//             $.each(results.items, function(index, item) {
-//                 $.get("tpl/item.html", function(data) {
-//                     $("#results").append(tplawesome(data, [{"title":item.snippet.title, "date":item.snippet.publishedAt}]));
-//                     // console.log(date[0]);
-//                 });
-//             });
-//             resetVideoHeight();
-//         });
-//     });
-//
-//     $(window).on("resize", resetVideoHeight);
-// });
-//
-// function resetVideoHeight() {
-//     $(".video").css("height", $("#results").width() * 9/16);
-// }
-//
-//
-//         function init() {
-//     gapi.client.setApiKey("AIzaSyD1Ej0tLQoT3TnhoWcVLlXaQlrULtzQwdQ");
-//     gapi.client.load("youtube", "v3", function() {
-//     });
-// }
-
-
-
-
 function tplawesome(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){return t[n][r]})}return res}
 
 
@@ -90,5 +42,34 @@ function resetVideoHeight() {
 function init() {
     gapi.client.setApiKey("AIzaSyD1Ej0tLQoT3TnhoWcVLlXaQlrULtzQwdQ");
     gapi.client.load("youtube", "v3", function() {
+        // $(function() {
+        //     $("#search2").on("click", function(e) {
+        //         e.preventDefault();
+        //         // prepare the request
+        //         var request = gapi.client.youtube.search.list({
+        //             part: "snippet",
+        //             type: "video",
+        //             videoCategoryId: 28,
+        //             q: "Javascript|Python -basics",
+        //             maxResults: 15,
+        //             order: "date",
+        //             publishedAfter: "2019-04-08T00:00:00Z"
+        //         });
+        //         request.execute(function(response) {
+        //             var results = response.result;
+        //             // console.log(results);
+        //             $("#results").html("");
+        //             $.each(results.items, function(index, item) {
+        //                 $.get("tpl/item.html", function(data) {
+        //                     let date = moment(item.snippet.publishedAt).format('YYYY-MM-DD, h:mm:ss');
+        //                     // let Newdate = date.replace(/\.\w*/g,"");
+        //                     console.log(date);
+        //                     $("#results").append(tplawesome(data, [{"title":item.snippet.title,  "videoid":item.id.videoId, "date":date}]));
+        //                 });
+        //             });
+        //             resetVideoHeight();
+        //         });
+        //     });
+        // console.log("Api is ready")
     });
 }
